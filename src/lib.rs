f186691a9a46de237de88799c26ff3826ea43e60
snylonue <snylonue@gmail.com>
2023-11-30 23:28:56 +0800

    get rid of cell

--- src/lib.rs
+++ src/lib.rs
@@ -1,71 +1,65 @@
-use sugars::cell;
 use std::collections::BTreeMap;
 use std::iter::FromIterator;
-use std::cell::Cell;
 
-#[derive(Default, Clone, Copy, Debug)]
-pub struct Range {
-    pub l: usize,
-    pub r: usize,
-}
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct ChthollyTree<T> {
-    inner: BTreeMap<Cell<Range>, T>,
+    inner: BTreeMap<usize, (usize, T)>,
     len: usize,
 }
 
-impl Range {
-    pub const fn new(l: usize, r: usize) -> Self {
-        Self { l, r }
-    }
-}
-impl PartialEq for Range {
-    fn eq(&self, other: &Self) -> bool {
-        self.l.eq(&other.l)
-    }
-}
-impl PartialOrd for Range {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.l.partial_cmp(&other.l)
-    }
-}
-impl Eq for Range {}
-impl Ord for Range {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.l.cmp(&other.l)
-    }
-}
 impl<T> ChthollyTree<T> {
     pub fn new() -> Self {
-        Self { inner: Default::default(), len: 0 }
+        Self {
+            inner: Default::default(),
+            len: 0,
+        }
     }
+
     pub const fn len(&self) -> usize {
         self.len
     }
+
+    pub const fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
 }
+
 impl<T: Eq> ChthollyTree<T> {
     pub fn push(&mut self, value: T) {
-        match self.inner.iter().next_back() {
-            Some((range, v)) if v == &value => {
-                let rg = unsafe { &*range.as_ptr() };
-                range.set(Range::new(rg.l, rg.r + 1));
+        match self.inner.last_entry() {
+            Some(mut entry) if entry.get().1 == value => {
+                entry.get_mut().0 += 1;
             }
-            _ => { self.inner.insert(cell!(Range::new(self.len, self.len)), value); }
+            _ => {
+                self.inner.insert(self.len, (self.len + 1, value));
+            }
         }
         self.len += 1;
     }
 }
+
 impl<T: Clone> ChthollyTree<T> {
+    /// splits a node [l, r) into [l, at) and [at, r)
+    /// # Panic
+    ///
+    /// panic if `at` >= `len`
     pub fn split(&mut self, at: usize) {
-        let (target, value) = self.inner.range(..=cell!(Range::new(at, 0))).next_back().unwrap();
-        let Range { l, r } = target.get();
-        if at != r {
-            let value = value.to_owned();
-            target.set(Range::new(l, at - 1));
-            self.inner.insert(cell!(Range::new(at, r)), value);
+        debug_assert!(at < self.len());
+        if at == 0 {
+            return;
+        }
+
+        let (_, (r, val)) = self.inner.range_mut(..=at).next_back().unwrap();
+        let rb = *r;
+
+        if at != rb {
+            let value = val.clone();
+            *r = at;
+            self.inner.insert(at, (rb, value));
         }
     }
 }
+
 impl<T: Eq> FromIterator<T> for ChthollyTree<T> {
     fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
         let mut tree = Self::new();
